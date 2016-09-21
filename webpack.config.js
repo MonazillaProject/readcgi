e9e9ca99226ba34aec2a20be5c63e48a829aefeb
New 2ch <webmaster@n2ch.ml>
2016-09-21 21:36:45 +0900

    Refactor some files

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,25 @@
 module.exports = {
-  entry: './src/main.js',
-  output: {
-    path: "./build",
-    filename: 'build.js'
-  },
-  module: {
-    loaders: [
-      {
-        test: /\.vue$/,
-        exclude: /node_modules/,
-        loader: 'vue'
-      },
-      {
-        test: /\.css$/,
-        loader: 'style!css'
-      },
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader',
-        query: {
-          cacheDirectory: true,
-          presets: ['es2015']
-        }
-      }
-    ]
-  }
+    entry: './src/main.js',
+    output: {
+        path: "./build",
+        filename: 'build.js'
+    },
+    module: {
+        loaders: [{
+            test: /\.vue$/,
+            exclude: /node_modules/,
+            loader: 'vue'
+        }, {
+            test: /\.css$/,
+            loader: 'style!css'
+        }, {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader',
+            query: {
+                cacheDirectory: true,
+                presets: ['es2015']
+            }
+        }]
+    }
 };
